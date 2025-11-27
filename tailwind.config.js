fd387423ef0f1f5fb604740e0ad0e6a37dbd86ed
SamBunheng <sbunheng708@gmail.com>
2025-11-27 11:26:02 +0700

    Add hero slider and responsive styles to landing page

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    screens: {
+      mobile: '320px',   // mobile starts at 320px
+      tablet: '481px',   // tablet starts at 481px
+      desktop: '1025px', // desktop starts at 1025px
+    },
+  },
+  plugins: [],
+}
